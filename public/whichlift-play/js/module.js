344d2bf0ff4dc0d6be5111ad0cddac7e89369340
Jefferson Wu <jefwu@uslaxssf9202.global.publicisgroupe.net>
2017-12-26 01:04:53 -0800

    working on randomizing which lift

---
+++ public/whichlift-play/js/module.js
@@ -0,0 +1,15 @@
+// dummy module to assess module loading in chrome.
+
+function getJSON (url, callback) {
+    let xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+        callback(this.responseText);
+    };
+
+    xhr.open('GET', url, true);
+    xhr.send();
+}
+//
+// export function getUsefulContents(url, callback) {
+//     getJSON(url, (data) => callback(JSON.parse(data)));
+// }
---
+++ public/whichlift-play/js/whichlift-play-es6.js
@@ -0,0 +1,6 @@
+//whichlift-play-es6.js
+
+console.log('whichlift-play-es6.js loaded.');
+
+
+
