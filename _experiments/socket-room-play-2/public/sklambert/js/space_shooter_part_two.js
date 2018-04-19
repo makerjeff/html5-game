4bad35987a70c2dbcbde0e4993cc69216f054d0d
Jefferson Wu <jefwu@uslaxssf9202.global.publicisgroupe.net>
2018-04-19 12:41:51 -0700

    up to Pool

--- _experiments/socket-room-play-2/public/sklambert/js/space_shooter_part_two.js
+++ _experiments/socket-room-play-2/public/sklambert/js/space_shooter_part_two.js
@@ -51,7 +51,7 @@ function Drawable() {
         this.y = y;
         this.width = width;
         this.height = height;
-    }
+    };
 
     this.speed = 0;
     this.canvasWidth = 0;
@@ -92,6 +92,64 @@ Background.prototype = new Drawable();
 
 
 // tutorial 2 main objects here
+
+/**
+ * Custom Pool object. Holds Bullet objects to be managed to prevent garbage collection.
+ */
+
+function Pool(maxSize) {
+    var size = maxSize; // Max bullets allowed in the pool.
+    var pool = [];
+
+    // Populates the pool array with Bullet object
+    this.init = function() {
+        for (var i = 0; i < size; i++) {
+            var bullet = new Bullet();  // to be created
+            bullet.init(0,0, imageRepository.bullet.width, imageRepository.bullet.height);
+            pool[i] = bullet;   // different way of pushing object to array.
+        }
+    };
+
+    /**
+     * Grabs the last item in the list and initializes it and pushes to front of array.
+     */
+    this.get = function(x, y, speed) {
+        if (!pool[size-1].alive) {
+            pool[size-1].spawn(x, y, speed);
+            pool.unshift(pool.pop());
+        }
+    };
+
+    /**
+     * Used for the ship to be able to get two bullets at once.
+     * If only the get() function is used twice, the ship is able to
+     * fire and only have 1 bullet spawn instead of 2.
+     */
+    this.getTwo = function(x1, y1, speed1, x2, y2, speed2) {
+        if (!pool[size-1].alive && !pool[size-2].alive) {
+            this.get (x1, y1, speed1);
+            this.get(x2, y2, speed2);
+        }
+    };
+
+    /**
+     * Draws any in-use Bullet objects. If a bullet goes off the screen,
+     * clear it and push it to the front of the array.
+     */
+
+    this.animate = function() {
+        for (var i = 0; i < size; i++) {
+            if (pool[i].alive) {
+                if (pool[i].draw()) {
+                    pool[i].clear();
+                    pool.push((pool.splice(i,1))[0]);
+                }
+            } else {
+                break;
+            }
+        }
+    };
+}
 
 
 
