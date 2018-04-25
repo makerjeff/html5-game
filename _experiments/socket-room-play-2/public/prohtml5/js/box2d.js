4a504d1ed426125027277f1caf737f7cc4a0c2e1
Jefferson Wu <jefwu@uslaxssf9202.global.publicisgroupe.net>
2018-04-24 17:53:28 -0700

    continuing box2d

---
+++ _experiments/socket-room-play-2/public/prohtml5/js/box2d.js
@@ -0,0 +1,24 @@
+// -- BOX2D --
+
+// declare all commonly used objects as variables for convenience
+let b2Vec2              = Box2D.Common.Math.b2Vec2;
+let b2BodyDef           = Box2D.Dynamics.b2BodyDef;
+let b2Body              = Box2D.Dynamics.b2Body;
+let b2FixtureDef        = Box2D.Dynamics.b2FixtureDef;
+let b2Fixture           = Box2D.Dynamics.b2Fixture;
+let b2World             = Box2D.Dynamics.b2World;
+let b2PolygonShape      = Box2D.Collision.Shapes.b2PolygonShape;
+let b2CircleShape       = Box2D.Collision.Shapes.b2CircleShape;
+let b2DebugDraw         = Box2D.Dynamics.b2DebugDraw;
+let b2RevoluteJointDef  = Box2D.Dynamics.Joints.b2RevoluteJointDef;
+
+let world;
+let scale = 30; // 30 pixels on canvas = 1 meter in Box2d world
+
+// jeffnote: init() ~= boilerplate.
+function init() {
+    // Set up the Box2d world that will do most of the physics calculation
+    let gravity = new b2Vec2(0, 9.8);   // gravity, 9.8 m/s^2 downward
+    let allow_sleep = true;     // Allow objects that are at rest to fall asleep and exclude from calculations.
+    world = new b2World(gravity, allow_sleep);
+}
